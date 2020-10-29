964b20fc15661e98b4d24c7faa7616c86993c64e
Flaviano Castiglia <flaviano.castiglia@altran.it>
2020-10-29 17:44:25 +0100

    creazione progetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-routing-app';
+  name = '';
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+    });
+  }
+
 }
